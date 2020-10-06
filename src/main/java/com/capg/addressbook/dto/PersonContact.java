b8e161c113ebe0adc660aea7e742365d0505d139
beni2412 <hardaman97@gmail.com>
2020-10-07 01:59:56 +0530

    [Hardaman] Added: Feature to search people in a City or state across multiple address books

--- src/main/java/com/capg/addressbook/dto/PersonContact.java
+++ src/main/java/com/capg/addressbook/dto/PersonContact.java
@@ -9,71 +9,87 @@ public class PersonContact {
 	private Long zip;
 	private String phone;
 	private String email;
-	
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public void setZip(Long zip) {
 		this.zip = zip;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public Long getZip() {
 		return zip;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "First Name: "+firstName+"\nLast Name: "+lastName+"\nAddress: "+address+"\nCity: "+city+"\nState: "+state+"\nZip: "+zip+"\nPhone Number: "+phone+"\nEmail: "+email+"\n\n";
+		return "First Name: " + firstName + "\nLast Name: " + lastName + "\nAddress: " + address + "\nCity: " + city
+				+ "\nState: " + state + "\nZip: " + zip + "\nPhone Number: " + phone + "\nEmail: " + email + "\n\n";
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		boolean result;
-		if(obj==null) {
+		if (obj == null) {
 			result = false;
-		}
-		else {
-			PersonContact person = (PersonContact)obj;
+		} else {
+			PersonContact person = (PersonContact) obj;
 			result = this.firstName.equalsIgnoreCase(person.firstName);
 		}
-		
+
 		return result;
 	}
 }
--- src/main/java/com/capg/addressbook/service/AddressBookService.java
+++ src/main/java/com/capg/addressbook/service/AddressBookService.java
@@ -3,16 +3,15 @@ package com.capg.addressbook.service;
 import com.capg.addressbook.dto.AddressBook;
 
 public interface AddressBookService {
-	
+
 	public void findPerson();
-	
+
 	public void updatePerson();
-	
+
 	public void deletePerson();
-	
+
 	public void showOptions(AddressBook addressBook);
-	
+
 	public AddressBook createAddressBook(String name);
-	
 
 }
